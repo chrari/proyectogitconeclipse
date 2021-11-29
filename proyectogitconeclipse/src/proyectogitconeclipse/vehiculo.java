f7ecd2017dd5d95aee1d74f5df55b59f13af9784
cmacoromines <cmacoromines@dominio.com>
2021-11-29 17:03:41 +0100

    commit prueba

---
+++ proyectogitconeclipse/src/proyectogitconeclipse/vehiculo.java
@@ -0,0 +1,11 @@
+package proyectogitconeclipse;
+
+public class vehiculo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("vehiculo");
+		String nombre="Christofer";
+	}
+
+}
